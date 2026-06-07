b447ca83498b0159667f1bbf2e2a0cea08542d95
Gevans4352 <emogoghena@gmail.com>
2026-06-07 23:50:55 +0100

    feat(backend): setup express app with middleware and routes

--- void/Backend/app.js
+++ void/Backend/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const authRoutes = require('./Routes/auth')
+const fragmentRoutes = require('./Routes/fragments');
+
+
+
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/fragments', fragmentRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Void API is alive' })
+})
 
 module.exports = app;
