56f9c58703bc374862558f1e1f250d2f077d8a6b
Gevans4352 <emogoghena@gmail.com>
2026-06-08 20:25:21 +0100

    feat(app): register all routes to express app

--- void/Backend/app.js
+++ void/Backend/app.js
@@ -1,20 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const authRoutes = require('./Routes/auth')
-const fragmentRoutes = require('./Routes/fragments');
-
-
-
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./Routes/auth");
+const fragmentRoutes = require("./Routes/fragments");
+const constellationRoutes = require("./Routes/constellations");
+const groveRoutes = require("./Routes/grove");
+const carouselRoutes = require("./Routes/carousel");
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/fragments', fragmentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/fragments", fragmentRoutes);
+app.use("/api/constellations", constellationRoutes);
+app.use("/api/grove", groveRoutes);
+app.use("/api/carousel", carouselRoutes);
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Void API is alive' })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Void API is alive" });
+});
 
 module.exports = app;
